548c069cf0003f7793f23cf0a4e6ced6cf3ab005
ManiyaNehal19 <maniyanehal@gmail.com>
2026-01-29 06:48:16 +0500

    depl err2

--- src/app/api/thread/[id]/route.ts
+++ src/app/api/thread/[id]/route.ts
@@ -3,18 +3,23 @@ import connectionToDataBase from "@/lib/mongodb";
 import Thread from "@/models/message";
 import { auth } from "@clerk/nextjs/server";
 
-export const GET = async (req: NextRequest, { params }: { params: { id: string } }) => {
+export async function GET(
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
   try {
     await connectionToDataBase();
 
     const { userId } = await auth();
     console.log(userId, "Get function");
+
     if (!userId) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-    const { id } = await params;
+    const { id } = await context.params;
     console.log("Fetching thread with ID:", id);
+
     const thread = await Thread.findOne({ thread_id: id, userId });
 
     if (!thread) {
@@ -26,4 +31,4 @@ export const GET = async (req: NextRequest, { params }: { params: { id: string }
     console.error("Failed to get thread:", err);
     return NextResponse.json({ error: "Server error" }, { status: 500 });
   }
-};
+}
